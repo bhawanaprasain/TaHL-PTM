64148dc97ff2e6d231125d819eb7dd8ae760cb76
bhawanaprasain <bhawana.prs@gmail.com>
2026-08-13 08:38:46 -0400

    Bash scripts for ft

---
+++ bash_scripts/progen_full_ft.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+CONFIG=utils/configs.json
+
+export PYTHONPATH=$pwd:$PYTHONPATH
+
+
+window_size=$(python3 -c "import json; print(json.load(open('$CONFIG'))['max_seq_len'])")
+rank=$(python3 -c "import json; print(json.load(open('$CONFIG'))['rank'])")
+progen_model_size=$(python3 -c "import json; print(json.load(open('$CONFIG'))['progen_model_size'])")
+
+ptm_alias=$(python3 -c "import json; print(json.load(open('$CONFIG'))['ptm_alias'])")
+lora_alpha=$(python3 -c "import json; print(json.load(open('$CONFIG'))['lora_alpha'])")
+num_of_epochs=$(python3 -c "import json; print(json.load(open('$CONFIG'))['num_of_epochs'])")
+motif_mode=$(python3 -c "import json; print(json.load(open('$CONFIG'))['motif_mode'])")
+
+target_modules=$(python3 -c "import json; cfg=json.load(open('$CONFIG')); print(''.join(cfg['target_modules']))")
+
+echo "$target_modules"
+
+
+mkdir -p training_logs
+mkdir -p training_logs/full_finetuned_progen_${progen_model_size}/${ptm_alias}
+
+nohup python3 -u src/progen/full_ft.py > training_logs/full_finetuned_progen_${progen_model_size}/${ptm_alias}/motif_marker_${motif_mode}_window_${window_size}_num_of_epochs_${num_of_epochs}.log 2>&1 &
+
